af8bb6a348d085bbc649c28d204607f8ead75029
zahidkumar <55661840+zahidkumar@users.noreply.github.com>
2021-02-09 15:40:55 +0530

    Update push.sh

--- jenkins/pipeline/jenkins/push/push.sh
+++ jenkins/pipeline/jenkins/push/push.sh
@@ -11,4 +11,4 @@ docker login -u ricardoandre97 -p $PASS
 echo "*** Tagging image ***"
 docker tag $IMAGE:$BUILD_TAG ricardoandre97/$IMAGE:$BUILD_TAG
 echo "*** Pushing image ***"
-docker push ricardoandre97/$IMAGE:$BUILD_TAG
+docker push zahid2021/$IMAGE:$BUILD_TAG
