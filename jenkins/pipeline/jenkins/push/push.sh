0829dd957bcf8a1e457d617b579717bd110311ef
zahidkumar <55661840+zahidkumar@users.noreply.github.com>
2021-02-09 15:48:17 +0530

    Update push.sh

--- jenkins/pipeline/jenkins/push/push.sh
+++ jenkins/pipeline/jenkins/push/push.sh
@@ -7,8 +7,8 @@ echo "********************"
 IMAGE="maven-project"
 
 echo "** Logging in ***"
-docker login -u ricardoandre97 -p $PASS
+docker login -u zahid2021 -p $PASS
 echo "*** Tagging image ***"
-docker tag $IMAGE:$BUILD_TAG ricardoandre97/$IMAGE:$BUILD_TAG
+docker tag $IMAGE:$BUILD_TAG zahid2021/$IMAGE:$BUILD_TAG
 echo "*** Pushing image ***"
 docker push zahid2021/$IMAGE:$BUILD_TAG
