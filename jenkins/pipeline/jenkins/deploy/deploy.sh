0242cf5e53c54a5f482dafc936fff5884fbcd174
zahidkumar <55661840+zahidkumar@users.noreply.github.com>
2021-02-09 15:51:55 +0530

    Update deploy.sh

--- jenkins/pipeline/jenkins/deploy/deploy.sh
+++ jenkins/pipeline/jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /opt/prod /tmp/.auth prod-user@linuxfacilito.online:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish prod-user@linuxfacilito.online:/tmp/publish
-ssh -i /opt/prod prod-user@linuxfacilito.online "/tmp/publish"
+scp -i /opt/prod /tmp/.auth ec2-user@65.1.84.146:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish ec2-user@65.1.84.146:/tmp/publish
+ssh -i /opt/prod ec2-user@65.1.84.146 "/tmp/publish"
